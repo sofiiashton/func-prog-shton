7adb837d69cb5127ea122692ed605c4bcdad80dd
sofiiashton <shtonsofia23@gmail.com>
2023-11-19 21:44:02 -0500

    upd

---
+++ haskell-projects/Lab6/app/Main.hs
@@ -0,0 +1,17 @@
+module Main (main) where
+
+import Lib
+
+main :: IO ()
+main = print (fac 6)
+
+fix :: (a -> a) -> a
+fix f = let x = f x in x
+
+fac' :: (Integer -> Integer) -> Integer -> Integer
+fac' f n =
+  if n == 0 then 1
+  else n * f (n - 1)
+
+fac :: Integer -> Integer
+fac = fix fac'
